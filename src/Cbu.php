51336eb3dce6b6991af1e22fe0d4e62fdd1756ed
Pablo Reyes <>
2014-10-31 18:05:16 -0300

    Mayor descripción de las funciones.

--- src/Cbu.php
+++ src/Cbu.php
@@ -1,27 +1,35 @@
 <?php
 /**
- * Cbu
+ * Permite validar los CBU (Clave Bancaria Uniforme), Argentina.
  *
  * @author Pablo Gabriel Reyes
  * @link https://pabloreyes.com.ar/ Blog
  * @link https://github.com/pablorsk/cbu-validator-php CBU validator on GitHub
  * @version 1.0.0
  * 
- * Basado en Toba (https://repositorio.siu.edu.ar/)
+ * Basado en Toba, de https://repositorio.siu.edu.ar/
  */
 
 class Cbu
 {
+	/**
+	 * Devuelve true si el CBU $cbu es válido.
+	 * Controla longitud, caracteres y número verificador de acuerdo a lo 
+	 * expuesto por Banco Central Argentino (http://www.bcra.gov.ar/pdfs/snp/SNP3002.pdf)
+	 * 
+	 * @param string $cbu
+	 * @return boolean 
+	 */
 	public static function isValid($cbu)
 	{
 		// Estrictamente sólo 22 números
 		if (!preg_match('/[0-9]{22}/', $cbu))
 			return false;
 		
-		$digitos_cbu = str_split($cbu);
-		if ($digitos_cbu[7] <> self::_verif($digitos_cbu, 0, 6))
+		$arr = str_split($cbu);
+		if ($arr[7] <> self::getDigitoVerificador($arr, 0, 6))
 			return false;
-		if ($digitos_cbu[21] <> self::_verif($digitos_cbu, 8, 20))
+		if ($arr[21] <> self::getDigitoVerificador($arr, 8, 20))
 			return false;
 		
 		return true;
@@ -36,7 +44,8 @@ class Cbu
 	 * @param integer $pos_final 
 	 * @return integer digito verificador de la cadena $numero
 	 */
-	private function _verif($numero, $pos_inicial, $pos_final){
+	private function getDigitoVerificador($numero, $pos_inicial, $pos_final)
+	{
 		$ponderador = array(3,1,7,9);
 		$suma = 0;
 		$j = 0;
@@ -45,7 +54,6 @@ class Cbu
 			$suma = $suma + ($numero[$i] * $ponderador[$j % 4]);
 			$j++;
 		}
-		$digito = (10 - $suma % 10) % 10;
-		return $digito;
+		return (10 - $suma % 10) % 10;
 	}
 }
